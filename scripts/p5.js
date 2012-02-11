4b5005a3015d8b0cf2b7ac4f95776022e7e4078d
K. Scott Allen <scott@OdeToCode.com>
2012-02-11 17:28:31 -0500

    part 1

---
+++ scripts/p5.js
@@ -0,0 +1,17 @@
+﻿var p5 = function () {
+    "use strict";
+
+    var start = function () {        
+        setFirstVisibleSlide();
+    };
+
+    var setFirstVisibleSlide = function () {
+        $("section").first.css("current");
+    };
+
+    return {
+        start: start
+    };
+
+} ();
+
